3cbb28d7e7848621c74ace355101664bc3c7d359
santiago <stenahe@gmail.com>
2026-06-04 13:39:34 +0200

    feat: add cache to mapb

--- command_mapb.go
+++ command_mapb.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/santiagotena/pokedexcli/internal/pokeapi"
@@ -11,14 +12,28 @@ func commandMapb(cfg *config) error {
 		fmt.Println("you're on the first page")
 		return nil
 	}
-
 	url := *cfg.prevLocationsURL
 
-	prev, next, _, err := pokeapi.FetchLocations(url)
+	cache, ok := cfg.cache.Get(url)
+	if ok {
+		locationAreas := pokeapi.LocationAreas{}
+		err := json.Unmarshal(cache, &locationAreas)
+		if err != nil {
+			fmt.Println("Error marshalling response body")
+			return err
+		}
+		for _, result := range locationAreas.Results {
+			fmt.Println(result.Name)
+		}
+		cfg.prevLocationsURL = locationAreas.Previous
+		cfg.nextLocationsURL = locationAreas.Next
+		return nil
+	}
+
+	prev, next, _, err := pokeapi.FetchLocationAreas(url)
 	if err != nil {
 		return err
 	}
-
 	cfg.prevLocationsURL = prev
 	cfg.nextLocationsURL = next
 
