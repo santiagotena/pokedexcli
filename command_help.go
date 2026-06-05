b1ae5c229db6dd2918efbd300a14882d4d65b605
santiago <stenahe@gmail.com>
2026-06-05 17:56:42 +0200

    feat: expand output

--- command_help.go
+++ command_help.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func commandHelp(cfg *config, param1 string) error {
 	helpMessage :=
-		"Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nmap: Displays the next location areas\nmapb: Displays the previous location areas\nexplore <area_name>: Displays the Pokemons available in the input and available area\nexit: Exit the Pokedex"
+		"Welcome to the Pokedex!\n" +
+			"Usage:\n\n" +
+			"help: Displays a help message\n" +
+			"map: Displays the next location areas\n" +
+			"mapb: Displays the previous location areas\n" +
+			"explore <area_name>: Displays the Pokemon available in the input and available area\n" +
+			"catch <pokemon>: Attempts to catch a Pokemon\n" +
+			"inspect <pokemon>: Details the stats of a caught Pokemon\n" +
+			"pokedex: List all the caught Pokemon\n" +
+			"exit: Exit the Pokedex\n"
 
-	fmt.Println(helpMessage)
+	fmt.Print(helpMessage)
 	return nil
 }
